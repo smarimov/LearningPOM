fd3011a6011697eafaa89c7f0782a8ef239803cf
smarimov <marimovshokirbek@gmail.com>
2021-05-28 13:11:42 -0400

    Page Object Modelling by Shokirbek

---
+++ Config/config.py
@@ -0,0 +1,8 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:\\Program Files\\Python39\\chromedriver.exe"
+
+    BASE_URL = 'https://app.hubspot.com/login'
+    USER_NAME = 'shokir92@gmail.com'
+    PASSWORD = 'Thelevelup123#'
+    LOGIN_PAGE_TITLE = "HubSpot Login"
+
